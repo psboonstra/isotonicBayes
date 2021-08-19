d9939a11c1ca16c58f0e96ec4c4ede3acddf650a
psboonstra <philb@umich.edu>
2021-08-19 09:17:33 -0400

    recreating with rstan_create_package

--- R/count_stan_divergences.R
+++
@@ -1,5 +0,0 @@
-count_stan_divergences = function(stan_fit) {
-  foo = get_sampler_params(stan_fit, inc_warmup = FALSE);
-  n_draws = lapply(foo, nrow)[[1]];
-  sum(unlist(lapply(foo,"[",i = 1:n_draws, j = "divergent__")));
-}
